19b3a7d295e4da01307a4de97981b0e308d8184b
Pool <pool.zm@gmail.com>
2024-11-19 11:55:32 -0500

    actualizacion web

--- src/components/InteractiveMapMobil.jsx
+++ src/components/InteractiveMapMobil.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useRef } from "react";
+import React, { useRef, useState } from "react";
 import MapSvg from "./MapSvg";
 
-const InteractiveMap = () => {
-const formRef = useRef(null);
+const InteractiveMapMobil = () => {
+  const formRef = useRef(null);
   const [loading, setLoading] = useState(false);
   const [showAmenidades, setShowAmenidades] = useState(false);
   const [progress, setProgress] = useState(0);
@@ -117,72 +117,60 @@ const formRef = useRef(null);
   };
 
   return (
-    <div
-className="bg-[url('https://res.cloudinary.com/dnwshzyqp/image/upload/v1726896332/manarola/fondoMapaInteractivo_hv2mnb.webp')] bg-no-repeat h-full w-[1280px] relative overflow-hidden"
-      style={{ backgroundSize: "100% 100%" }}
-    >
-      <div
-        className={`w-full h-full bg-[url('https://res.cloudinary.com/dnwshzyqp/image/upload/v1726429870/manarola/AmenidadesSinNada_vvpsas.png')] bg-no-repeat transition-opacity duration-300 ${
-          showAmenidades ? "opacity-100" : "opacity-0"
-        }`}
-        style={{ backgroundSize: "100% 100%" }}
-      ></div>
-      <MapSvg
-        manzana={selectedManzana}
-        lote={selectedLote}
-        className={
-          "h-full w-[1280px] absolute -left-[5px] -top-[2px] -rotate-[0.1deg]"
-        }
-      />
-      <div
-className={`bg-white rounded-3xl ${
-          showAmenidades ? "w-[50px] min-h-[5px]" : "w-[530px] min-h-[340px]"
-        } absolute bottom-5 right-28 px-10 py-5`}
->
+    <div>
+      <div className="w-full overflow-x-scroll">
+        <div className="relative max-w-[1200px]">
+          <img
+            src="https://res.cloudinary.com/dnwshzyqp/image/upload/v1726896332/manarola/fondoMapaInteractivo_hv2mnb.webp"
+            alt=""
+            className="max-w-[1200px]"
+          />
+          <div
+            className={`w-[1200px] h-full bg-[url('https://res.cloudinary.com/dnwshzyqp/image/upload/v1726429870/manarola/AmenidadesSinNada_vvpsas.png')] bg-no-repeat transition-opacity duration-300 absolute top-0 left-0 ${
+              showAmenidades ? "opacity-100" : "opacity-0"
+            }`}
+            style={{ backgroundSize: "100% 100%" }}
+          ></div>
+          <MapSvg manzana={selectedManzana} lote={selectedLote} className={"h-full w-[1200px] absolute -left-[1.6px] -top-[2.5px] "} />
+        </div>
+      </div>
+      <div className={`bg-white rounded-tl-3xl p-3 relative w-full`}>
         <button
-className="uppercase text-sm text-white bg-button tracking-widest px-8 py-4 rounded-l-3xl rounded-tr-3xl absolute right-0 -top-8"
+          className="uppercase text-sm text-white bg-button tracking-widest px-8 py-5 rounded-l-3xl rounded-tr-3xl absolute right-0 -top-8"
           onClick={handleAmenidadesShow}
         >
-          {showAmenidades ? "Ocultar" : "          Mostrar"} amenidades
+          {showAmenidades ? "Ocultar" : "Mostrar"} amenidades
         </button>
-        <div
-className={`inline-flex justify-around gap-x-2 mt-5 w-full ${
-            showAmenidades ? "hidden" : ""
-          }`}
->
-          <span className="font-semibold text-black text-sm pl-[2.7rem] relative before:w-10 before:h-5 before:rounded-xl before:bg-[#10FFE8] before:absolute before:left-0">
+        <div className={`inline-flex justify-around gap-x-1 w-full mt-7`}>
+          <span className="font-semibold text-black text-xs pl-[2rem] relative before:w-8 before:h-4 before:rounded-xl before:bg-[#10FFE8] before:absolute before:left-0">
             110 m<sup>2</sup>
           </span>
-          <span className="font-semibold text-black text-sm pl-[2.7rem] relative before:w-10 before:h-5 before:rounded-xl before:bg-[#00B7FF] before:absolute before:left-0">
+          <span className="font-semibold text-black text-xs pl-[2rem] relative before:w-8 before:h-4 before:rounded-xl before:bg-[#00B7FF] before:absolute before:left-0">
             130 m<sup>2</sup>
           </span>
-          <span className="font-semibold text-black text-sm pl-[2.7rem] relative before:w-10 before:h-5 before:rounded-xl before:bg-[#256AF4] before:absolute before:left-0">
+          <span className="font-semibold text-black text-xs pl-[2rem] relative before:w-8 before:h-4 before:rounded-xl before:bg-[#256AF4] before:absolute before:left-0">
             150 m<sup>2</sup>
           </span>
-<span className="font-semibold text-black text-sm pl-[2.7rem] relative before:w-10 before:h-5 before:rounded-xl before:bg-[#ff0037] before:absolute before:left-0">
+          <span className="font-semibold text-black text-xs pl-[2rem] relative before:w-8 before:h-4 before:rounded-xl before:bg-[#ff0037] before:absolute before:left-0">
             Vendido
           </span>
         </div>
         <div
-className={`inline-flex justify-around gap-x-5 w-full mt-5 ${
-            showAmenidades ? "hidden" : ""
-          }`}
->
-          <h2 className="uppercase text-zinc-400 font-bold text-sm tracking-widest">
+          className={`inline-flex justify-around gap-x-5 w-full mt-5`}
+        >
+          <h2 className="uppercase text-zinc-400 font-bold text-xs tracking-widest">
             Manzana
           </h2>
-          <h2 className="uppercase text-zinc-400 font-bold text-sm tracking-widest">
+          <h2 className="uppercase text-zinc-400 font-bold text-xs tracking-widest">
             Lote
           </h2>
-          <h2 className="uppercase text-zinc-400 font-bold text-sm tracking-widest">
+          <h2 className="uppercase text-zinc-400 font-bold text-xs tracking-widest">
             Finalizar
           </h2>
         </div>
         <div
-className={`w-[85%] mx-auto h-3 bg-zinc-200 rounded-lg relative mt-5 ${
-            showAmenidades ? "hidden" : ""
-          }`}
->
+          className={`w-[85%] mx-auto h-3 bg-zinc-200 rounded-lg relative mt-5`}
+        >
           <div
             className="h-full bg-button rounded-md transition-all duration-300"
             style={{ width: `${progress}%` }}
@@ -206,17 +194,13 @@ className={`w-[85%] mx-auto h-3 bg-zinc-200 rounded-lg relative mt-5 ${
             style={{ left: "99%" }}
           ></div>
         </div>
-        <div
-          className={`w-full text-center mt-5 ${
-            showAmenidades ? "hidden" : ""
-          }`}
-        >
+        <div className={`w-full text-center mt-5`}>
           {progress === 0 && (
             <>
               <h2 className="text-black text-lg font-light">
                 Selecciona tu manzana
               </h2>
-              <div className="inline-flex gap-x-5 mt-3">
+              <div className="inline-flex gap-x-3 mt-3">
                 {POINTERS.map((pointer, index) => (
                   <div
                     key={index}
@@ -224,7 +208,7 @@ className={`w-[85%] mx-auto h-3 bg-zinc-200 rounded-lg relative mt-5 ${
                       selectedManzana === pointer.manzana
                         ? "bg-green-500"
                         : "bg-black"
-                    } rounded-full w-12 h-12 flex items-center justify-center cursor-pointer`}
+                    } rounded-full w-10 h-10 flex items-center justify-center cursor-pointer`}
                     onClick={() => {
                       setSelectedManzana(pointer.manzana);
                       setLotes(pointer.lote);
@@ -243,13 +227,13 @@ className={`w-[85%] mx-auto h-3 bg-zinc-200 rounded-lg relative mt-5 ${
               <h2 className="text-black text-lg font-light">
                 Selecciona tu lote
               </h2>
-              <div className="flex flex-wrap justify-center gap-x-5 gap-y-3 mt-3 w-full">
+              <div className="flex flex-wrap justify-center gap-x-3 gap-y-2 mt-3 w-full">
                 {lotes.map((lote, index) => (
                   <div
                     key={index}
                     className={`${
                       selectedLote === lote.number ? "bg-green-500" : lote.color
-                    } rounded-full w-12 h-12 flex items-center justify-center cursor-pointer ${
+                    } rounded-full w-10 h-10 flex items-center justify-center cursor-pointer ${
                       lote.number === "V" && "pointer-events-none"
                     }`}
                     onClick={() => {
@@ -266,30 +250,30 @@ className={`w-[85%] mx-auto h-3 bg-zinc-200 rounded-lg relative mt-5 ${
           )}
           {progress === 100 && (
             <form ref={formRef}>
-              <input
-                type="text"
-                name="Nombre completo"
-                className="w-full mt-4 border border-zinc-300 rounded-xl p-4 text-base mb-3"
-                placeholder="Nombre completo"
-                onChange={(e) => setName(e.target.value)}
-              />
-              <input
-                type="text"
-                name="Número de celular"
-                className="w-full border border-zinc-300 rounded-xl p-4 text-base"
-                placeholder="Número de celular"
-                onChange={(e) => setTel(e.target.value)}
-              />
-              <input type="hidden" name="Lote" value={selectedLote} />
-              <input type="hidden" name="MZ" value={selectedManzana} />
-            </form>
+            <input
+              type="text"
+              name="Nombre completo"
+              className="w-full mt-4 border border-zinc-300 rounded-xl p-4 text-base mb-3"
+              placeholder="Nombre completo"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="text"
+              name="Número de celular"
+              className="w-full border border-zinc-300 rounded-xl p-4 text-base"
+              placeholder="Número de celular"
+              onChange={(e) => setTel(e.target.value)}
+            />
+            <input type="hidden" name="Lote" value={selectedLote} />
+            <input type="hidden" name="MZ" value={selectedManzana} />
+          </form>
           )}
         </div>
         <div className="flex justify-between mt-7">
           <button
             type="button"
             className={`uppercase text-sm text-white bg-button tracking-widest px-10 py-3 rounded-3xl mx-auto block ${
-              showAmenidades || progress === 0 ? "hidden" : ""
+              progress === 0 ? "hidden" : ""
             }`}
             onClick={handleBackClick}
           >
@@ -301,8 +285,8 @@ className={`w-[85%] mx-auto h-3 bg-zinc-200 rounded-lg relative mt-5 ${
               showAmenidades || progress === 100 ? "hidden" : ""
             }`}
             onClick={handleNextClick}
-        >
-          Siguiente
+          >
+            Siguiente
           </button>
           <button
             type="button"
@@ -337,8 +321,8 @@ className={`w-[85%] mx-auto h-3 bg-zinc-200 rounded-lg relative mt-5 ${
             }}
           >
             {loading ? "Enviando..." : "Enviar"}
-        </button>
-{/* <a
+          </button>
+          {/* <a
             href={`https://wa.me/51983792957?text=Hola%2C%20estoy%20interesado%20en%20la%20manzana%20${selectedManzana}%20y%20el%20lote%20${selectedLote}%20mi%20nombre%20es%20${name}%20y%20mi%20teléfono%20es%20${tel}%20.%20%20Gracias.%20`}
             target="_blank"
             className={`uppercase text-sm text-white bg-button tracking-widest px-10 py-3 rounded-3xl mx-auto block ${
@@ -365,4 +349,4 @@ className={`w-[85%] mx-auto h-3 bg-zinc-200 rounded-lg relative mt-5 ${
   );
 };
 
-export default InteractiveMap;
+export default InteractiveMapMobil;
